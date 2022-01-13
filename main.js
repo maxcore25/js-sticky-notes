42b92ab2d778e374cecbc04de42bd075a5d9f649
maxcore25 <mkorobov2002@mail.ru>
2022-01-13 15:54:30 +0300

    Add New Note Function completely done

--- main.js
+++ main.js
@@ -1,11 +1,64 @@
-function getNotes() {}
+const notesContainer = document.querySelector('#app');
+const addNoteButton = notesContainer.querySelector('.add-note');
 
-function saveNotes(notes) {}
+getNotes().forEach(note => {
+  const noteElement = createNoteElement(note.id, note.content);
+  notesContainer.insertBefore(noteElement, addNoteButton);
+});
 
-function createNoteElement(id, content) {}
+addNoteButton.addEventListener('click', () => addNote());
 
-function addNote() {}
+function getNotes() {
+  return JSON.parse(localStorage.getItem('stickynotes-notes') || '[]');
+}
 
-function updateNote(id, newContent) {}
+function saveNotes(notes) {
+  localStorage.setItem('stickynotes-notes', JSON.stringify(notes));
+}
 
-function deleteNote(id, element) {}
+function createNoteElement(id, content) {
+  const element = document.createElement('textarea');
+
+  element.classList.add('note');
+  element.value = content;
+  element.placeholder = 'Empty Sticky Note';
+
+  element.addEventListener('change', () => {
+    updateNote(id, element.value);
+  });
+
+  element.addEventListener('dblclick', () => {
+    const doDelete = confirm(
+      'Are you sure you wish to delete this sticky note?'
+    );
+    if (doDelete) {
+      deleteNote(id, element);
+    }
+  });
+
+  return element;
+}
+
+function addNote() {
+  const notes = getNotes();
+  const noteObject = {
+    id: Math.floor(Math.random() * 1000000),
+    content: '',
+  };
+
+  const noteElement = createNoteElement(noteObject.id, noteObject.content);
+  notesContainer.insertBefore(noteElement, addNoteButton);
+
+  notes.push(noteObject);
+  saveNotes(notes);
+}
+
+function updateNote(id, newContent) {
+  console.log('Updating note');
+  console.log(id, newContent);
+}
+
+function deleteNote(id, element) {
+  console.log('Deleting note');
+  console.log(id, element);
+}
