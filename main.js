3a589a25b84fd6790afade2e24e435a3edfe90a3
maxcore25 <mkorobov2002@mail.ru>
2022-01-13 14:51:42 +0300

    Notes and Add Button stylized

---
+++ main.js
@@ -0,0 +1,11 @@
+function getNotes() {}
+
+function saveNotes(notes) {}
+
+function createNoteElement(id, content) {}
+
+function addNote() {}
+
+function updateNote(id, newContent) {}
+
+function deleteNote(id, element) {}
